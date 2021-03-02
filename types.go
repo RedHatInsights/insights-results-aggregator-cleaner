3f09f4684db6ef709d26c32f6d504871e97f1a61
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-02 10:40:06 +0100

    Updated comments in types.go

--- types.go
+++ types.go
@@ -18,19 +18,23 @@ package main
 
 // Definition of custom data types used by this tool.
 
-// ClusterName represents name of cluster in format c8590f31-e97e-4b85-b506-c45ce1911a12
+// ClusterName represents name of cluster in format
+// c8590f31-e97e-4b85-b506-c45ce1911a12 (it must be proper UUID).
 type ClusterName string
 
-// ClusterList represents a list of cluster names/ids (see ClusterName)
+// ClusterList represents a list of cluster names/ids (see ClusterName data
+// type declared above)
 type ClusterList []ClusterName
 
-// TableAndKey represents a key for given table used by cleanup process
+// TableAndKey represents a key for given table used by cleanup process. Each
+// row is deleted by specifying table name and a key
 type TableAndKey struct {
 	TableName string
 	KeyName   string
 }
 
-// Summary represents summary info to be displayed after cleanup part
+// Summary represents summary info to be displayed in a table after cleanup
+// part
 type Summary struct {
 	ProperClusterEntries   int
 	ImproperClusterEntries int
