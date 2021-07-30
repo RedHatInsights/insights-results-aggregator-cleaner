42a1243f98776ff1cc4785c58a3d9e5dc28a72f0
Daniele Pensiero <daniele.pensiero@gmail.com>
2021-07-30 15:30:40 +0200

    test ci jenkins jobs

--- pr_check.sh
+++ pr_check.sh
@@ -12,7 +12,7 @@ IQE_MARKER_EXPRESSION="smoke"
 IQE_FILTER_EXPRESSION=""
 
 
-# Install bonfire repository and initialize
+# Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
